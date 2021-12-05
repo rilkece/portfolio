0bd4ccf28641057922e03eeb2173035f17cb500a
rilkece <erickle.luc@gmail.com>
2021-12-05 03:11:59 -0300

    Updates

---
+++ precache-manifest.eaf4a7f1c9fe5d3b16395ab550e74915.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04b0d863caa9b1b1235245ffda9df4c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "da0a0385c70676775efa",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "bac359f86c06a226db04",
+    "url": "/portfolio/static/css/main.3655595c.chunk.css"
+  },
+  {
+    "revision": "da0a0385c70676775efa",
+    "url": "/portfolio/static/js/2.232fbd69.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.232fbd69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac359f86c06a226db04",
+    "url": "/portfolio/static/js/main.3ff74763.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
